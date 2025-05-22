f7a535e2802961c2366c611f981db1a7a3812196
nasuoa <17995588+nasuoa@users.noreply.github.com>
2025-05-22 14:32:12 +1200

    Initial commit

---
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -0,0 +1,25 @@
+package nz.ac.auckland.se281;
+
+import java.util.List;
+
+/** This class is the main entry point. */
+public class MapEngine {
+
+  public MapEngine() {
+    // add other code here if you wan
+    loadMap(); // keep this mehtod invocation
+  }
+
+  /** invoked one time only when constracting the MapEngine class. */
+  private void loadMap() {
+
+    List<String> countries = Utils.readCountries();
+    List<String> adjacencies = Utils.readAdjacencies();
+  }
+
+  /** this method is invoked when the user run the command info-country. */
+  public void showInfoCountry() {}
+
+  /** this method is invoked when the user run the command route. */
+  public void showRoute() {}
+}
